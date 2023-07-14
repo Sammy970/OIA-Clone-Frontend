b8d5c57df2627cd322ede1359d5dca96cf9eacf2
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-14 20:55:03 +0530

    Test Drawer for Mobile

--- src/components/Dashboard/Sections/Section2.js
+++ src/components/Dashboard/Sections/Section2.js
@@ -29,71 +29,55 @@ const Section2 = () => {
   const isMobile = useBreakpointValue({ base: true, lg: false });
   const drawerPlacement = isMobile ? "bottom" : "left";
   const drawerSize = isMobile ? "xs" : "sm";
-  
+
   return (
     <>
       <Button leftIcon={<AddIcon />} colorScheme="teal" onClick={onOpen}>
         Create Link
       </Button>
-      <Drawer
-        isOpen={isOpen}
-        placement={drawerPlacement}
-        initialFocusRef={firstField}
-        onClose={onClose}
-        size={drawerSize}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader borderBottomWidth="1px">Create a new link</DrawerHeader>
+      {isMobile ? (
+        <Drawer
+          isOpen={isOpen}
+          placement={"top"}
+          initialFocusRef={firstField}
+          onClose={onClose}
+          size="sm"
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader borderBottomWidth="1px">
+              Create a new link
+            </DrawerHeader>
 
-          <DrawerBody>
-            <Stack spacing="24px">
-              <Box>
-                <FormLabel htmlFor="username">Name</FormLabel>
-                <Input
-                  ref={firstField}
-                  id="username"
-                  placeholder="Please enter user name"
-                />
-              </Box>
+            <DrawerBody>
+              <Stack spacing="24px">
+                <Box>
+                  <FormLabel htmlFor="url">Url</FormLabel>
+                  <InputGroup>
+                    <InputLeftAddon>https://</InputLeftAddon>
+                    <Input
+                      ref={firstField}
+                      type="url"
+                      id="url"
+                      placeholder="Please enter domain"
+                    />
+                  </InputGroup>
+                </Box>
+              </Stack>
+            </DrawerBody>
 
-              <Box>
-                <FormLabel htmlFor="url">Url</FormLabel>
-                <InputGroup>
-                  <InputLeftAddon>http://</InputLeftAddon>
-                  <Input
-                    type="url"
-                    id="url"
-                    placeholder="Please enter domain"
-                  />
-                  <InputRightAddon>.com</InputRightAddon>
-                </InputGroup>
-              </Box>
-
-              <Box>
-                <FormLabel htmlFor="owner">Select Owner</FormLabel>
-                <Select id="owner" defaultValue="segun">
-                  <option value="segun">Segun Adebayo</option>
-                  <option value="kola">Kola Tioluwani</option>
-                </Select>
-              </Box>
-
-              <Box>
-                <FormLabel htmlFor="desc">Description</FormLabel>
-                <Textarea id="desc" />
-              </Box>
-            </Stack>
-          </DrawerBody>
-
-          <DrawerFooter borderTopWidth="1px">
-            <Button variant="outline" mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme="blue">Submit</Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+            <DrawerFooter borderTopWidth="1px">
+              <Button variant="outline" mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="blue">Submit</Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      ) : (
+        <p>hello</p>
+      )}
     </>
   );
 };

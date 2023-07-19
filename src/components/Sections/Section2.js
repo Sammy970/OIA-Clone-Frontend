ef4acc985d9edce2e2eba5ca1ac570e5e1a1023c
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-19 21:52:43 +0530

    Testing with framer motion animation

--- src/components/Sections/Section2.js
+++ src/components/Sections/Section2.js
@@ -1,89 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Section2.module.css";
 import { Card, CardBody, Text } from "@chakra-ui/react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Container } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 const Section2 = () => {
+  const [comeCenter, setComeCenter] = useState(false);
+
   return (
     <section className={classes.section2}>
       <div className={classes.title}>Features</div>
-      <Container
-        maxW={"xl"}
-        // display={"flex"}
-        // justifyContent={"center"}
-        // alignItems={"center"}
-      >
+      <Container maxW={"xl"}>
         <Grid templateColumns="repeat(2, 1fr)" gap={3}>
           <GridItem>
-            <Card height="200px" background={"#0F0F0F"}>
-              <CardBody>
-                <Text
-                  position="absolute"
-                  bottom="4"
-                  left="3"
-                  right="3"
-                  fontSize={"22.9px"}
-                  color={"#FFFFFF"}
-                  whiteSpace="pre-line" // Add this line
-                  fontFamily="circular-std-medium"
-                >
-                  Effortless Link <br />
-                  Shortening
-                </Text>
-              </CardBody>
-            </Card>
+            <motion.div
+              drag
+              dragConstraints={{
+                top: 0,
+                left: 0,
+                right: 50,
+                bottom: 50,
+              }}
+              onClick={() => setComeCenter(!comeCenter)}
+            >
+              <Card height="200px" background={"#0F0F0F"}>
+                <CardBody>
+                  <Text
+                    position="absolute"
+                    bottom="4"
+                    left="3"
+                    right="3"
+                    fontSize={"22.9px"}
+                    color={"#FFFFFF"}
+                    whiteSpace="pre-line" // Add this line
+                    fontFamily="circular-std-medium"
+                  >
+                    Effortless Link <br />
+                    Shortening
+                  </Text>
+                </CardBody>
+              </Card>
+            </motion.div>
           </GridItem>
           <GridItem>
-            <Card height="200px">
-              <CardBody>
-                <Text
-                  position="absolute"
-                  bottom="4"
-                  left="3"
-                  right="3"
-                  fontSize={"22.9px"}
-                  whiteSpace="pre-line" // Add this line
-                  fontFamily="circular-std-medium"
-                >
-                  Customize <br /> Short URLs
-                </Text>
-              </CardBody>
-            </Card>
+            <motion.div
+              drag
+              dragConstraints={{
+                top: 0,
+                left: 50,
+                right: 0,
+                bottom: 50,
+              }}
+            >
+              <Card height="200px">
+                <CardBody>
+                  <Text
+                    position="absolute"
+                    bottom="4"
+                    left="3"
+                    right="3"
+                    fontSize={"22.9px"}
+                    whiteSpace="pre-line" // Add this line
+                    fontFamily="circular-std-medium"
+                  >
+                    Customize <br /> Short URLs
+                  </Text>
+                </CardBody>
+              </Card>
+            </motion.div>
           </GridItem>
           <GridItem>
-            <Card height="200px">
-              <CardBody>
-                <Text
-                  position="absolute"
-                  bottom="4"
-                  left="3"
-                  right="9"
-                  fontSize={"22.9px"}
-                  fontFamily="circular-std-medium"
-                >
-                  User Friendly
-                  <br /> Dash-board
-                </Text>
-              </CardBody>
-            </Card>
+            <motion.div
+              drag
+              dragConstraints={{
+                top: 50,
+                left: 0,
+                right: 50,
+                bottom: 0,
+              }}
+            >
+              <Card height="200px">
+                <CardBody>
+                  <Text
+                    position="absolute"
+                    bottom="4"
+                    left="3"
+                    right="9"
+                    fontSize={"22.9px"}
+                    fontFamily="circular-std-medium"
+                  >
+                    User Friendly
+                    <br /> Dash-board
+                  </Text>
+                </CardBody>
+              </Card>
+            </motion.div>
           </GridItem>
           <GridItem>
-            <Card height="200px" background={"#0F0F0F"}>
-              <CardBody>
-                <Text
-                  position="absolute"
-                  bottom="4"
-                  left="3"
-                  right="9"
-                  fontSize={"22.9px"}
-                  color={"#FFFFFF"}
-                  fontFamily="circular-std-medium"
-                >
-                  Powerful Link <br /> Analytics
-                </Text>
-              </CardBody>
-            </Card>
+            <motion.div
+              drag
+              dragConstraints={{
+                top: 50,
+                left: 50,
+                right: 0,
+                bottom: 0,
+              }}
+            >
+              <Card height="200px" background={"#0F0F0F"}>
+                <CardBody>
+                  <Text
+                    position="absolute"
+                    bottom="4"
+                    left="3"
+                    right="9"
+                    fontSize={"22.9px"}
+                    color={"#FFFFFF"}
+                    fontFamily="circular-std-medium"
+                  >
+                    Powerful Link <br /> Analytics
+                  </Text>
+                </CardBody>
+              </Card>
+            </motion.div>
           </GridItem>
         </Grid>
       </Container>

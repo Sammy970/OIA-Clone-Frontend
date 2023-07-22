907c3b064d00d33dec10296eb5677eae9516ccda
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-22 19:32:14 +0530

    Added Image in Section1 of FrontPage

--- src/components/Sections/Section1.js
+++ src/components/Sections/Section1.js
@@ -1,18 +1,33 @@
 import React from "react";
 import classes from "./Section1.module.css";
-import { Container } from "@chakra-ui/react";
+import { Container, Image, Stack } from "@chakra-ui/react";
+import headImage from "../../Assets/section1.png";
 
 const Section1 = () => {
   return (
     <section className={classes.section1}>
-      <Container maxW={"900px"}>
-        <h3>Your Shortcut to Easy Sharing and Snappy URLs!</h3>
-        <p>
-          Give personality to your{" "}
-          <span style={{ textDecoration: "underline" }}>links</span> and{" "}
-          <span style={{ textDecoration: "underline" }}>control</span> how they
-          are seen.
-        </p>
+      <Container maxW={"1100px"}>
+        <Stack
+          direction={"row"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          gap={10}
+        >
+          <Stack direction={"column"}>
+            <h3>Your Shortcut to Easy Sharing and Snappy URLs!</h3>
+            <p>
+              Give personality to your{" "}
+              <span style={{ textDecoration: "underline" }}>links</span> and{" "}
+              <span style={{ textDecoration: "underline" }}>control</span> how
+              they are seen.
+            </p>
+          </Stack>
+          <Image
+            src={headImage}
+            display={{ base: "none", md: "block" }}
+            width={"50%"}
+          />
+        </Stack>
       </Container>
     </section>
   );
